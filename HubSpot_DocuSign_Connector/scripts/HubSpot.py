9f8e3347be0222d772e6a3ea3abf77fd3466b708
antoineleducmusic <antoineleducmusic@gmail.com>
2023-03-13 09:34:36 -0400

    Update HubSpot.py

--- HubSpot_DocuSign_Connector/scripts/HubSpot.py
+++ HubSpot_DocuSign_Connector/scripts/HubSpot.py
@@ -5,7 +5,7 @@ from hubspot.crm.deals import BasicApi, AssociationsApi, BatchApi, SearchApi, Pu
 
 deal_id = '12451140480'
 
-api_client = HubSpot(access_token='pat-na1-8e4a5abf-3f2c-4d2b-9a7d-aec0c869f86b')
+api_client = HubSpot(access_token='')
 
 deal_fetched = api_client.crm.deals.basic_api.get_by_id(deal_id)
 
